20cbdfbb8447167c1d2a5bd4d3b2accaacd4f8fe
tmmteam <teamtmm2@gmail.com>
2026-04-13 19:40:21 +0530

    🥀ᴀʏᴜsʜ🥀

--- Clonify/platforms/Youtube.py
+++ Clonify/platforms/Youtube.py
@@ -7,9 +7,9 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
-from Ayush.utils.formatters import time_to_seconds
+from Clonify.utils.formatters import time_to_seconds
 import aiohttp
-from Ayush import LOGGER
+from Clonify import LOGGER
 from typing import Union
 
 YOUR_API_URL = None
